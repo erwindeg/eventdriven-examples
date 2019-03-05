3865295b8e9d4b0b8237d1058c8442aa505bdba4
Erwin de Gier <erwin.degier@trifork.nl>
2019-03-05 12:10:58 +0100

    added order aggregate test

---
+++ trading/src/test/java/nl/trifork/coins/trading/command/OrderAggregateTest.java
@@ -0,0 +1,48 @@
+package nl.trifork.coins.trading.command;
+
+import nl.trifork.coins.coreapi.*;
+import org.axonframework.test.aggregate.AggregateTestFixture;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.math.BigDecimal;
+
+public class OrderAggregateTest {
+
+    private AggregateTestFixture<OrderAggregate> fixture;
+
+    @Before
+    public void setUp() {
+        fixture = new AggregateTestFixture<>(OrderAggregate.class);
+    }
+
+    @Test
+    public void createOrder() {
+        fixture.givenNoPriorActivity()
+                .when(new CreateOrderCommand("orderId", "userId", "EUR", "BTC", BigDecimal.ONE, BigDecimal.TEN))
+                .expectEvents(new OrderCreatedEvent("orderId", "userId", "EUR", "BTC", BigDecimal.ONE, BigDecimal.TEN));
+    }
+
+    @Test
+    public void executeOrder() {
+        fixture.givenCommands(new CreateOrderCommand("orderId", "userId", "EUR", "BTC", BigDecimal.ONE, BigDecimal.TEN))
+                .when(new ExecuteOrderCommand("orderId","userId"))
+                .expectEvents(new OrderExecutedEvent("orderId", "userId", "EUR", "BTC", BigDecimal.ONE, BigDecimal.TEN));
+    }
+
+    @Test
+    public void successOrder() {
+        fixture.givenCommands(new CreateOrderCommand("orderId", "userId", "EUR", "BTC", BigDecimal.ONE, BigDecimal.TEN),
+                new ExecuteOrderCommand("orderId","userId"))
+                .when(new SuccessOrderCommand("orderId"))
+                .expectEvents(new OrderSuccessEvent("orderId"));
+    }
+
+    @Test
+    public void failOrder() {
+        fixture.givenCommands(new CreateOrderCommand("orderId", "userId", "EUR", "BTC", BigDecimal.ONE, BigDecimal.TEN),
+                new ExecuteOrderCommand("orderId","userId"))
+                .when(new FailOrderCommand("orderId"))
+                .expectEvents(new OrderFailedEvent("orderId"));
+    }
+}
