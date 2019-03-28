283691fcdf840757edc58722e2e2aefc61ef8bd4
Erwin de Gier <erwin.degier@trifork.nl>
2019-03-28 13:38:16 +0100

    use spy

--- rest-facade/src/test/java/nl/trifork/coins/ReactiveOperatorsTest.java
+++ rest-facade/src/test/java/nl/trifork/coins/ReactiveOperatorsTest.java
@@ -1,53 +1,58 @@
 package nl.trifork.coins;
 
-import org.assertj.core.util.Arrays;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
-import java.time.Duration;
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.TimeoutException;
 import java.util.function.Consumer;
 
-import static java.time.Duration.*;
+import static java.time.Duration.ofMillis;
+import static java.time.Duration.ofSeconds;
+import static org.mockito.AdditionalAnswers.delegatesTo;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.eq;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.timeout;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 
 @RunWith(MockitoJUnitRunner.class)
 public class ReactiveOperatorsTest {
 
-    @Mock
     Consumer consumer;
 
     @Mock
     Consumer errorConsumer;
 
+
+    @Before
+    public void setup() {
+        this.consumer = spyLambda(System.out::println);
+    }
+
     /*
-    * Mono subscribe()
-    * */
+     * Mono subscribe()
+     * */
     @Test
-    public void subscribeShouldCallConsumer1time(){
+    public void subscribeShouldCallConsumer1time() {
         Mono<Integer> intMono = Mono.just(1);
         intMono.subscribe(consumer);
 
         verify(consumer, times(1)).accept(any(Integer.class));
     }
 
-
     /*
      * Flux subscribe()
      * */
     @Test
     public void subscribeShouldCallConsumer10times() {
-        Flux<Integer> rangeFlux = Flux.range(0,10);
-        rangeFlux
-                .doOnEach(value -> System.out.println(value))
-                .subscribe(consumer);
+        Flux<Integer> rangeFlux = Flux.range(0, 10);
+        rangeFlux.subscribe(consumer);
+
 
         verify(consumer, times(10)).accept(any(Integer.class));
     }
@@ -59,22 +64,20 @@ public class ReactiveOperatorsTest {
     public void mapShouldConvertIntToString() {
         Mono<Integer> intMono = Mono.just(1);
         intMono
-                .map(item -> "Number: "+item)
+                .map(item -> "Number: " + item)
                 .subscribe(consumer);
 
         verify(consumer, times(1)).accept(any(String.class));
     }
-
 
     /*
      * Flux map()
      * */
     @Test
     public void mapShouldConvertIntsToStrings() {
-        Flux<Integer> rangeFlux = Flux.range(0,10);
+        Flux<Integer> rangeFlux = Flux.range(0, 10);
         rangeFlux
-                .map(item -> "Number: "+item)
-                .doOnEach(value -> System.out.println(value))
+                .map(item -> "Number: " + item)
                 .subscribe(consumer);
 
         verify(consumer, times(10)).accept(any(String.class));
@@ -85,10 +88,9 @@ public class ReactiveOperatorsTest {
      * */
     @Test
     public void mapShouldCalculateValues() {
-        Flux<Integer> rangeFlux = Flux.range(0,10);
+        Flux<Integer> rangeFlux = Flux.range(0, 10);
         rangeFlux
                 .map(this::someSuperFastCalculation)
-                .doOnEach(value -> System.out.println(value))
                 .subscribe(consumer);
 
         verify(consumer, times(10)).accept(any(Integer.class));
@@ -99,25 +101,22 @@ public class ReactiveOperatorsTest {
      * */
     @Test
     public void flatMapShouldCalculateValuesAsync() {
-        Flux<Integer> rangeFlux = Flux.range(0,10);
+        Flux<Integer> rangeFlux = Flux.range(0, 10);
         rangeFlux
                 .flatMap(this::someSuperLongCalculation)
-                .doOnEach(value -> System.out.println(value))
                 .subscribe(consumer);
 
         verify(consumer, times(10)).accept(any(Integer.class));
     }
-
 
     /*
      * Flux filter()
      * */
     @Test
     public void filterShouldOnlyPrintEventNumbers() {
-        Flux<Integer> rangeFlux = Flux.range(0,10);
+        Flux<Integer> rangeFlux = Flux.range(0, 10);
         rangeFlux
-                .filter(item -> item % 2 ==0)
-                .doOnEach(value -> System.out.println(value))
+                .filter(item -> item % 2 == 0)
                 .subscribe(consumer);
 
         verify(consumer, times(5)).accept(any(Integer.class));
@@ -127,11 +126,11 @@ public class ReactiveOperatorsTest {
      * Flux timeout()
      * */
     @Test
-    public void timeoutShouldThrowException(){
+    public void timeoutShouldThrowException() {
         Flux neverFlux = Flux.never();
         neverFlux
                 .timeout(ofSeconds(1))
-                .subscribe(consumer,errorConsumer);
+                .subscribe(consumer, errorConsumer);
 
         verify(consumer, times(0)).accept(any(String.class));
         verify(errorConsumer, timeout(1100)).accept(any(Exception.class));
@@ -141,24 +140,20 @@ public class ReactiveOperatorsTest {
      * Flux onErrorReturn
      * */
     @Test
-    public void timeoutShouldReturnDefaultValue(){
+    public void timeoutShouldReturnDefaultValue() {
         Flux neverFlux = Flux.never();
         neverFlux
                 .timeout(ofSeconds(1))
-                .doOnEach(value -> System.out.println(value))
                 .onErrorReturn("There was a timeout")
-                .doOnEach(value -> System.out.println(value))
-                .subscribe(consumer,errorConsumer);
+                .subscribe(consumer, errorConsumer);
 
         verify(consumer, timeout(1200)).accept(any(String.class));
         verify(errorConsumer, times(0)).accept(any(Exception.class));
     }
 
-
     @Test
-    public void combinedOperatorsShouldMatchSuccess(){
-        Flux.just("STARTED","PENDING","SUCCESS")
-                .doOnEach(value -> System.out.println(value))
+    public void combinedOperatorsShouldMatchSuccess() {
+        Flux.just("STARTED", "PENDING", "SUCCESS")
                 .filter(value -> "SUCCESS".equals(value))
                 .timeout(ofSeconds(1))
                 .next()
@@ -168,9 +163,8 @@ public class ReactiveOperatorsTest {
     }
 
     @Test
-    public void combinedOperatorsShouldMatchErrorForException(){
+    public void combinedOperatorsShouldMatchErrorForException() {
         Flux.<String>generate(sink -> sink.next("STARTED")).take(5).delayElements(ofMillis(500))
-                .doOnEach(value -> System.out.println(value))
                 .filter(value -> "SUCCESS".equals(value))
                 .timeout(ofSeconds(1))
                 .next()
@@ -180,10 +174,14 @@ public class ReactiveOperatorsTest {
     }
 
     private int someSuperFastCalculation(Integer value) {
-        return value*2;
+        return value * 2;
     }
 
-    private Flux<Integer> someSuperLongCalculation(Integer value){
-        return Flux.just(value*2);
+    private Flux<Integer> someSuperLongCalculation(Integer value) {
+        return Flux.just(value * 2);
+    }
+
+    private Consumer spyLambda(final Consumer lambda) {
+        return mock(Consumer.class, delegatesTo(lambda));
     }
 }
